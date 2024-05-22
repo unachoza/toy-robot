186dd2bcdd94cc03c093cb559360bfa7e554d503
arianna choza <unachoza@gmail.com>
2024-05-22 13:38:08 -0400

    fixing modal z index to also cover robot when open

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -9,18 +9,16 @@ interface ModalProps {
 
 const Modal = ({ toggling, content }: ModalProps) => {
 	return (
-		<div className="info-modal-container">
-			<div className="modal-container show-modal" id="modal">
-				<div className="modal">
-					<img
-						src="https://res.cloudinary.com/dh41vh9dx/image/upload/v1619580778/icons8-macos-close-32.png"
-						className="close-icon"
-						onClick={toggling}
-						alt="close icon"
-					/>
-					{content?.length < 200 && <div className="modal-header">Report</div>}
-					<ReactMarkdown className="modal-content">{content}</ReactMarkdown>
-				</div>
+		<div className="modal-container show-modal" id="modal">
+			<div className="modal">
+				<img
+					src="https://res.cloudinary.com/dh41vh9dx/image/upload/v1619580778/icons8-macos-close-32.png"
+					className="close-icon"
+					onClick={toggling}
+					alt="close icon"
+				/>
+				{content?.length < 200 && <div className="modal-header">Report</div>}
+				<ReactMarkdown className="modal-content">{content}</ReactMarkdown>
 			</div>
 		</div>
 	);
